b400f3fff85eb3dc8a6e2010e1a2a5ec401e47f6
Tom Van Braband <tom.vanbraband@sonarsource.com>
2021-02-11 09:34:25 +0100

    create project, submit a report, delete project

---
+++ sender.py
@@ -0,0 +1,34 @@
+import requests
+import time
+import os
+from requests.auth import HTTPBasicAuth
+import json
+
+token = os.environ['SC_TOKEN']
+project = os.environ['PROJECT_KEY']
+
+x = requests.post(f'https://dev.sc-dev.io/api/projects/create?organization=tamaragunkel&name={project}&project={project}',
+               auth=HTTPBasicAuth(token, ''))
+print(x.text)
+
+files = {'report': open(f'/Users/tom/Downloads/{project}.zip','rb')}
+x = requests.post(f'https://dev.sc-dev.io/api/ce/submit?projectKey={project}&organization=tamaragunkel',
+                    auth=HTTPBasicAuth(token, ''),
+                    files=files)
+taskId = json.loads(x.text)['taskId']
+
+status = "PENDING"
+while status != 'FAILED':
+    status = json.loads(requests.get(f'https://dev.sc-dev.io/api/ce/task?id={taskId}',
+               auth=HTTPBasicAuth(token, '')).text)['task']['status']
+    print(status)
+    time.sleep(1)
+
+x = requests.post(f'https://dev.sc-dev.io/api/projects/delete?project={project}',
+               auth=HTTPBasicAuth(token, ''))
+
+
+
+
+
+
